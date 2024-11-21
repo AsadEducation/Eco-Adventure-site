7c064f3ed35360b5ce6cf65755460e65f35ebe12
AsadEducation <AsadEducation@outlook.com>
2024-11-21 23:10:08 +0600

    added lazy loading and suspense to handle map issue in <MapComponent>

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, Suspense } from 'react';
 import Navbar from '../Components/Navbar';
 import Banner from '../Components/Banner';
 import Footer from '../Components/Footer';
@@ -7,7 +7,9 @@ import AdventureProvider from '../Provider/AdventureProvider';
 import { useLocation } from 'react-router-dom';
 import PageTitle from '../Components/PageTitle';
 import Faq from '../ExtraSection/Faq';
-import MapComponent from '../ExtraSection/MapComponent';
+// import MapComponent from '../ExtraSection/MapComponent';
+// this can cause error 
+const MapComponent = React.lazy(() => import('../ExtraSection/MapComponent'));
 
 
 const Home = () => {
@@ -15,18 +17,18 @@ const Home = () => {
     const location = useLocation();
     // console.log(location);
 
-    let path='';
+    let path = '';
 
-    if(location.pathname=='/'){
-      path='Home';
+    if (location.pathname == '/') {
+        path = 'Home';
     }
 
     return (
         <div>
-           <PageTitle title={`Eco-Adventure||${path}`}/>
+            <PageTitle title={`Eco-Adventure||${path}`} />
             <header>
                 {/* navbar */}
-               <div className='w-11/12 mx-auto'> <Navbar ></Navbar></div>
+                <div className='w-11/12 mx-auto'> <Navbar ></Navbar></div>
 
                 {/* banner  */}
                 <Banner></Banner>
@@ -35,14 +37,20 @@ const Home = () => {
                 {/* show different pages based on route  */}
 
                 <AdventureProvider>
-                    <Expo/>
+                    <Expo />
                 </AdventureProvider>
 
-                <div className='my-8 md:my-16 lg:my-24'><Faq/></div>
+                <div className='my-8 md:my-16 lg:my-24'><Faq /></div>
 
                 {/* map component  */}
-                
-                <div><MapComponent/></div>
+
+                  {/* this can cause error  */}
+
+                <Suspense fallback={<div>Loading map...</div>}>
+                    <div><MapComponent /></div>
+                </Suspense>
+
+
 
             </main>
             <footer>
